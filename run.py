f9ae424871f615075d8eb47a21496ad9c0a68b64
nataliatesarova <naftalintesarova@gmail.com>
2023-04-01 14:22:32 +0000

    Add function to insert employee to sheet

--- run.py
+++ run.py
@@ -12,6 +12,12 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project3')
 
+def add_employee(sheet_name, data):
+    """
+    code to add a new employee into the sheet
+    """
+    sheet = SHEET.worksheet(sheet_name)
+    sheet.append_row(data)
 
 
 def main():
@@ -22,6 +28,11 @@ def main():
     sheet = SHEET.worksheet('Sheet1')
     data = sheet.get_all_values()
     print(data)
+
+    # add employee to sheet
+    add_employee('Sheet1', employee_data)
+
+
 
    
 
