47d37faaa0fdf498286ecaa94e19ef8359d5df30
nataliatesarova <naftalintesarova@gmail.com>
2023-04-09 17:55:32 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -12,10 +12,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project3')
 
+
 def validate_data(data):
+    """
+     Validation check of input data
+    """
     try:
         # data validation here
-        # check whether the user has entered 9 columns
+        # check whether the user has entered 9 data points
         data_list = data.split(',')
         data_col_length = len(data_list)
         if data_col_length != 9:
@@ -29,11 +33,10 @@ def validate_data(data):
         print('checking int conversion: ', int(data_id))
         print('checking int conversion: ', int(data_phone))
         print('checking int conversion: ', int(data_asalary))
-        # check = int(data_id) and int(data_phone) and int(data_asalary)
     except Exception:
-        return False 
-    
-    return True 
+        return False
+    return True
+
 
 def get_employee_data():
     """
@@ -53,8 +56,8 @@ def get_employee_data():
 
     employee_data = data.split(",")
     print('employee data is: ', employee_data)
-       
     return employee_data
+
 
 def add_employee(sheet_name, data):
     """
@@ -63,6 +66,25 @@ def add_employee(sheet_name, data):
     sheet = SHEET.worksheet(sheet_name)
     sheet.append_row(data)
 
+
+def delete_employee(sheet_name, employee_id):
+    """
+    code to delete an employee from the sheet
+    """
+    sheet = SHEET.worksheet(sheet_name)
+    data = sheet.get_all_values()
+    headers = data[0]
+    id_index = headers.index('ID')
+# find the row corresponding to the given employee ID
+    for row in data[1:]:
+        if row[id_index] == employee_id:
+            # delete the row and return True to indicate success
+            sheet.delete_rows(data.index(row) + 1)  # add 1 to account for header row
+            return True
+# if no employee with the given ID was found, return False to indicate failure
+    return False
+
+
 def main():
     """
     main function which has all business logic
@@ -70,13 +92,7 @@ def main():
     # accessing sheet 1 for employee data
     sheet = SHEET.worksheet('Sheet1')
     data = sheet.get_all_values()
-    print(data)
-
-    # call a function, pass data, inside the function calculate size of data which gives you the number of employees in the sheet
-    # get the total number of employees subtract 1 to exclude the header row
-    # number_employees = len(data) - 1
-    # print("The number of employees is " + str(number_employees))
-    # print(f"The number of employees is{number_employees}")
+    # print(data)
 
     # loop over the records in the data using for loop and calculate monthly salary for each employee
     first_row = data[0]
@@ -94,10 +110,6 @@ def main():
 
     employee_data = get_employee_data()
     # add employee to sheet
-    add_employee('Sheet1', employee_data)
-
-   
-
-# calling main function
+    add_employee('Sheet1', employee_data) 
+print("Welcome to Employee Data System")
 main()
-
