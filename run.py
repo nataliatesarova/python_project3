250b5afc8ffede6863b49c24408fad5a381dfe91
nataliatesarova <naftalintesarova@gmail.com>
2023-04-15 10:08:36 +0000

    Update print statements with spacing

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project3')
 
+
 def validate_data(data):
     """
      Validation check of input data
@@ -29,6 +30,7 @@ def validate_data(data):
         return False
     return True
 
+
 def get_employee_data():
     """
     function to get employee data from user input
@@ -36,8 +38,8 @@ def get_employee_data():
     # Prompt user to enter employee data
     print("Please enter employee data.")
     # Provide instructions on how to enter data
-    print("Enter data separated by comma in order of: ID,Forename,Surname,Email,Telephone number,Department,Position,Annual salary,Start date.")
-    print("Example: 10,Julian,Jones,julianjones@gmail.com,721878900,Marketing,Marketing Agent,38400,1.9.2020\n")
+    print("Enter data separated by comma in order of:\nID,Forename,Surname,Email,Telephone number,\nDepartment,Position,Annual salary,Start date.")
+    print("Example:\n10,Julian,Jones,julianjones@gmail.com,721878900,\nMarketing,Marketing Agent,38400,1.9.2020\n")
 
     # Get user input for employee data
     data = input("Enter your data\n")
@@ -63,6 +65,7 @@ def get_employee_data():
     # print confirmation message to user that employee data has been added
     print("Employee data added successfully")
 
+
 def add_employee(sheet_name, data):
     """
     code to add a new employee into the sheet
@@ -71,6 +74,7 @@ def add_employee(sheet_name, data):
     sheet = SHEET.worksheet(sheet_name)
     # Append a new row to the worksheet with the employee data
     sheet.append_row(data)
+
 
 def delete_employee():
     """
@@ -94,6 +98,7 @@ def delete_employee():
             return
     # If no match found, print message
     print("No matching row found")
+
 
 def search_employee_data():
     """
